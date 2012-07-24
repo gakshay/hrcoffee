3225cce8d484afcc89f21dc1fafd9ad2c5bbc558
Akshay Gupta <akshay.aries11@gmail.com>
2012-07-24 14:14:24 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hrcoffee::Application.config.secret_token = 'b609f6cdc381a30d2ccaf42e34b6bed5fcf5544130f29c8055f6aba25551d9e453eee8c5b416cd9b8ad8f6f000f3759cbdf469cc5048185a01ba15c91e8e8e27'
